ccfa6d489c41b8634e79248295e1f640dbc6b8c1
Anthony Lloyd <anthony@lloydhq.com>
2020-07-12 00:06:11 +0100

    readme

--- Tests/CheckTests.cs
+++ Tests/CheckTests.cs
@@ -79,23 +79,14 @@ namespace Tests
         //[Fact]
         //public void Version()
         //{
-        //    static bool ArrayEqual<T>(T[] a, T[] b) where T : IEquatable<T>
-        //    {
-        //        if (a.Length != b.Length) return false;
-        //        for (int i = 0; i < a.Length; i++)
-        //            if (!a[i].Equals(b[i])) return false;
-        //        return true;
-
-        //    }
         //    Gen.Select(Gen.Byte, Gen.Byte, Gen.Byte)
         //    .Select(t => new Version(t.V0, t.V1, t.V2))
-        //    .Array(0, 100)
+        //    .Array[0, 100]
         //    .Sample(expected =>
         //    {
         //        var actual = (Version[])expected.Clone();
         //        Array.Reverse(actual);
-        //        return ArrayEqual(expected, actual);
-        //        //Assert.Equal(expected, actual);
+        //        Assert.Equal(expected, actual);
         //    });
         //}
     }
