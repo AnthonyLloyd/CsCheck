339dc54bd05297bfcae6453d7a1c1fa6297e2721
Anthony Lloyd <anthony@lloydhq.com>
2020-08-24 20:28:35 +0100

    remove SQLite

--- Tests/ArraySerializerTests.cs
+++ Tests/ArraySerializerTests.cs
@@ -37,19 +37,6 @@ namespace Tests
             });
         }
 
-        [Fact]
-        public void SQLite()
-        {
-            Gen.UInt.Sample(i =>
-            {
-                var bytes = new byte[8];
-                int pos = 0;
-                ArraySerializer.WriteSQLite(bytes, ref pos, i);
-                pos = 0;
-                return ArraySerializer.ReadSQLite(bytes, ref pos) == i;
-            });
-        }
-
         void PrefixVarint_Faster(double skew)
         {
             var bytes = new byte[8];
@@ -72,30 +59,7 @@ namespace Tests
         [Fact]
         public void PrefixVarint_Faster_NoSkew() => PrefixVarint_Faster(0);
         [Fact]
-        public void PrefixVarint_Faster_Skew10() => PrefixVarint_Faster(10);
-
-
-        [Fact]
-        public void SQLite_Faster()
-        {
-            var bytes = new byte[8];
-            Gen.UInt.Faster(i =>
-            {
-                int pos = 0;
-                ArraySerializer.WriteSQLite(bytes, ref pos, i);
-                pos = 0;
-                return i == ArraySerializer.ReadSQLite(bytes, ref pos);
-            }
-            , i =>
-            {
-                int pos = 0;
-                ArraySerializer.WriteVarint(bytes, ref pos, i);
-                pos = 0;
-                return ArraySerializer.ReadVarint(bytes, ref pos) == i;
-            }
-            , threads: 1, sigma: 50, repeat: 10_000)
-            .Output(writeLine);
-        }
+        public void PrefixVarint_Faster_Skew10() => PrefixVarint_Faster(5);
     }
 
     public static class ArraySerializer
@@ -140,50 +104,6 @@ namespace Tests
                 if (b < 128u) return i + b;
                 i = (i + (b & 127u)) << 7;
             }
-        }
-
-        public static void WriteSQLite(byte[] bytes, ref int pos, uint val)
-        {
-            //const uint X = 128u;
-            //const uint Y= ((252u - X) << 8) + 256u + X;
-            if (val < 128u) bytes[pos++] = (byte)val;
-            else if (val < 32128u)
-            {
-                bytes[pos++] = (byte)(((val - 128u) >> 8) + 128u);
-                bytes[pos++] = (byte)(val - 128u);
-            }
-            else if (val < 0x10000u)
-            {
-                bytes[pos++] = 253;
-                bytes[pos++] = (byte)(val >> 8);
-                bytes[pos++] = (byte)val;
-            }
-            else if (val < 0x1000000u)
-            {
-                bytes[pos++] = 254;
-                bytes[pos++] = (byte)(val >> 16);
-                bytes[pos++] = (byte)(val >> 8);
-                bytes[pos++] = (byte)val;
-            }
-            else
-            {
-                bytes[pos++] = 255;
-                bytes[pos++] = (byte)(val >> 24);
-                bytes[pos++] = (byte)(val >> 16);
-                bytes[pos++] = (byte)(val >> 8);
-                bytes[pos++] = (byte)val;
-            }
-        }
-
-        public static uint ReadSQLite(byte[] bytes, ref int pos)
-        {
-            //const uint X = 128u;
-            uint A0 = bytes[pos++];
-            return A0 < 128u ? A0
-                 : A0 < 253u ? ((A0 - 128u) << 8) + 128u + bytes[pos++]
-                 : A0 == 253u ? ((uint)bytes[pos++] << 8) + bytes[pos++]
-                 : A0 == 254u ? ((uint)bytes[pos++] << 16) + ((uint)bytes[pos++] << 8) + bytes[pos++]
-                 : ((uint)bytes[pos++] << 24) + ((uint)bytes[pos++] << 16) + ((uint)bytes[pos++] << 8) + bytes[pos++];
         }
 
         public static void WritePrefixVarint(byte[] bytes, ref int pos, uint val)
