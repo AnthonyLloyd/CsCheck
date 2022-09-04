b10c618af0fe1e4ea6ecba02a4fd7a888050bcff
Anthony Lloyd <anthony@lloydhq.com>
2022-09-04 20:48:04 +0100

    simplify allocator

--- Tests/Allocator.cs
+++ Tests/Allocator.cs
@@ -45,9 +45,52 @@ public static class Allocator
                 index = i;
             }
             var increment = Math.Sign(residual);
-            residual -= increment;
             results[index] += increment;
+            residual -= increment;
         } while (residual != 0);
+        return results;
+    }
+
+    public static long[] AllocateBenchmark(long quantity, double[] weights)
+    {
+        var sumWeights = 0.0;
+        for (int i = 0; i < weights.Length; i++)
+            sumWeights += weights[i];
+        var results = new long[weights.Length];
+        var residual = quantity;
+        for (int i = 0; i < weights.Length; i++)
+        {
+            var allocation = (long)Math.Round(quantity * weights[i] / sumWeights, MidpointRounding.AwayFromZero);
+            results[i] = allocation;
+            residual -= allocation;
+        }
+        if (residual >= results.Length || residual <= -results.Length)
+            throw new Exception($"Numeric overflow, quantity={quantity}, weights={string.Join(',', weights)}, residual={residual}");
+        var increment = Math.Sign(residual);
+        while (residual != 0)
+        {
+            var minInc = double.MaxValue;
+            var maxWei = double.MinValue;
+            var minIndex = 0;
+            for (int i = 0; i < weights.Length; i++)
+            {
+                var weight = weights[i];
+                // the following gives better handling of calculation rounding errors than:
+                // allocation error:  var error = results[i] - quantity * weight / sumWeights;
+                // weight error:      var error = results[i] * sumWeights / quantity - weight;
+                // norm weight error: var error = results[i] / quantity - weight / sumWeights;
+                var error = results[i] * sumWeights - quantity * weight;
+                var inc = Math.Abs(error + increment * sumWeights) - Math.Abs(error); // increase in the error
+                if (inc > minInc) continue;
+                var wei = Math.Abs(weight);
+                if (inc == minInc && wei <= maxWei) continue;
+                minInc = inc;
+                maxWei = wei;
+                minIndex = i;
+            }
+            results[minIndex] += increment;
+            residual -= increment;
+        }
         return results;
     }
 }
--- Tests/AllocatorTests.cs
+++ Tests/AllocatorTests.cs
@@ -9,88 +9,91 @@ using Xunit;
 
 public class AllocatorTests
 {
-    readonly static Gen<(long Total, double[] Weights)> genAllSigns =
-        Gen.Select(Gen.Long[-100, 100], Gen.Double[0, 100, 100].Array[1, 30].Where(ws => Math.Abs(ws.Sum()) > 1e-9));
+    readonly Action<string> writeLine;
+    public AllocatorTests(Xunit.Abstractions.ITestOutputHelper output) => writeLine = output.WriteLine;
 
-    readonly static Gen<(long Total, double[] Weights)> genIntegerWeights =
-        Gen.Select(Gen.Long[-100, 100], Gen.Int[0, 100].Cast<double>().Array[1, 30].Where(ws => Math.Abs(ws.Sum()) > 1e-9));
+    readonly static Gen<(long Quantity, double[] Weights)> genAllSigns =
+        Gen.Select(Gen.Long[-1000, 1000], Gen.Double[-1000, 1000, 100].Array[1, 30].Where(ws => Math.Abs(ws.Sum()) > 1e-9));
+
+    readonly static Gen<(long Quantity, double[] Weights)> genIntegerWeights =
+        Gen.Select(Gen.Long[-1000, 1000], Gen.Int[-1000, 1000].Cast<double>().Array[1, 30].Where(ws => Math.Abs(ws.Sum()) > 1e-9));
 
     [Fact]
-    public void ErrorMinimising_TotalsCorrectly()
+    public void Allocate_TotalsCorrectly()
         => AllocatorCheck.TotalsCorrectly(genAllSigns, Allocator.Allocate);
 
     [Fact]
-    public void ErrorMinimising_BetweenFloorAndCeiling()
+    public void Allocate_BetweenFloorAndCeiling()
         => AllocatorCheck.BetweenFloorAndCeiling(genAllSigns, Allocator.Allocate);
 
     [Fact]
-    public void ErrorMinimising_SmallerWeightsDontGetLargerAllocation()
+    public void Allocate_SmallerWeightsDontGetLargerAllocation()
         => AllocatorCheck.SmallerWeightsDontGetLargerAllocation(genAllSigns, Allocator.Allocate);
 
     [Fact]
-    public void ErrorMinimising_GivesOppositeForNegativeTotal()
+    public void Allocate_GivesOppositeForNegativeTotal()
         => AllocatorCheck.GivesOppositeForNegativeQuantity(genAllSigns, Allocator.Allocate);
 
     [Fact]
-    public void ErrorMinimising_GivesSameForNegativeWeights()
+    public void Allocate_GivesSameForNegativeWeights()
         => AllocatorCheck.GivesSameForNegativeWeights(genAllSigns, Allocator.Allocate);
 
     [Fact]
-    public void ErrorMinimising_GivesOppositeForNegativeBoth()
+    public void Allocate_GivesOppositeForNegativeBoth()
         => AllocatorCheck.GivesOppositeForNegativeBoth(genAllSigns, Allocator.Allocate);
 
     [Fact]
-    public void ErrorMinimising_HasSmallestAllocationError()
+    public void Allocate_HasSmallestAllocationError()
         => AllocatorCheck.HasSmallestAllocationError(genAllSigns, Allocator.Allocate, true);
 
     [Fact(Skip = "Not true but interesting cases")]
-    public void ErrorMinimising_HasSmallestAllocationError_ExactForIntegerWeights()
+    public void Allocate_HasSmallestAllocationError_ExactForIntegerWeights()
         => AllocatorCheck.HasSmallestAllocationError(genIntegerWeights, Allocator.Allocate, false);
 
     [Fact]
-    public void ErrorMinimising_Twitter()
+    public void Allocate_Twitter()
     {
         var actual = Allocator.Allocate(100, new[] { 406.0, 348.0, 246.0, 0.0 });
         Assert.Equal(new long[] { 40, 35, 25, 0 }, actual);
     }
 
     [Fact]
-    public void ErrorMinimising_TwitterZero()
+    public void Allocate_TwitterZero()
     {
         var actual = Allocator.Allocate(0, new[] { 406.0, 348.0, 246.0, 0.0 });
         Assert.Equal(new long[] { 0, 0, 0, 0 }, actual);
     }
 
     [Fact]
-    public void ErrorMinimising_TwitterTotalNegative()
+    public void Allocate_TwitterTotalNegative()
     {
         var actual = Allocator.Allocate(-100, new[] { 406.0, 348.0, 246.0, 0.0 });
         Assert.Equal(new long[] { -40, -35, -25, -0 }, actual);
     }
 
     [Fact]
-    public void ErrorMinimising_TwitterWeightsNegative()
+    public void Allocate_TwitterWeightsNegative()
     {
         var actual = Allocator.Allocate(100, new[] { -406.0, -348.0, -246.0, -0.0 });
         Assert.Equal(new long[] { 40, 35, 25, 0 }, actual);
     }
 
     [Fact]
-    public void ErrorMinimising_TwitterBothNegative()
+    public void Allocate_TwitterBothNegative()
     {
         var actual = Allocator.Allocate(-100, new[] { -406.0, -348.0, -246.0, -0.0 });
         Assert.Equal(new long[] { -40, -35, -25, -0 }, actual);
     }
 
     [Fact]
-    public void ErrorMinimising_TwitterTricky()
+    public void Allocate_TwitterTricky()
     {
         var actual = Allocator.Allocate(100, new[] { 404.0, 397.0, 57.0, 57.0, 57.0, 28.0 });
         Assert.Equal(new long[] { 40, 39, 6, 6, 6, 3 }, actual);
     }
 
     [Fact]
-    public void ErrorMinimising_NegativeExample()
+    public void Allocate_NegativeExample()
     {
         var positive = Allocator.Allocate(42, new[] { 1.5, 1.0, 39.5, -1.0, 1.0 });
         var negative = Allocator.Allocate(-42, new[] { 1.5, 1.0, 39.5, -1.0, 1.0 });
@@ -98,18 +101,27 @@ public class AllocatorTests
     }
 
     [Fact]
-    public void ErrorMinimising_Exceptions()
+    public void Allocate_Exceptions()
     {
         Assert.Throws<Exception>(() => Allocator.Allocate(0, new[] { 0.0, 0.0, 0.0 }));
         Assert.Throws<Exception>(() => Allocator.Allocate(42, new[] { 1.0, -2.0, 1.0, 1e-30 }));
     }
 
     [Fact]
-    public void ErrorMinimising_Integer()
+    public void Allocate_Integer()
     {
         Assert.Equal(new long[] { -3, 1, 1 }, Allocator.Allocate(-1, new[] { -24.0, 6.0, 8.0 }));
         Assert.Equal(new long[] { 0, 0, -1 }, Allocator.Allocate(-1, new[] { -2.0, -4.0, 16.0 }));
         Assert.Equal(new long[] { -2, -2, 3 }, Allocator.Allocate(-1, new[] { 31.0, 19.0, -38.0 }));
         Assert.Equal(new long[] { 1, 4 }, Allocator.Allocate(5, new[] { 1.0, 9.0 }));
+    }
+
+    [Fact]
+    public void Allocate_Performance()
+    {
+        genAllSigns.Faster(
+            (q, w) => Allocator.Allocate(q, w),
+            (q, w) => Allocator.AllocateBenchmark(q, w)
+        , threads: 1, repeat: 100).Output(writeLine);
     }
 }
