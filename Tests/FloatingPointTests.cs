e5de674394921c0103b73176ddfcd4c6da5af5c8
Anthony Lloyd <anthony@lloydhq.com>
2023-11-25 14:30:38 +0000

    floating tests

--- Tests/FloatingPointTests.cs
+++ Tests/FloatingPointTests.cs
@@ -7,82 +7,59 @@ using Xunit;
 
 public class FloatingPointTests(Xunit.Abstractions.ITestOutputHelper output)
 {
-    readonly static Gen<long> genPriceLong = Gen.Long[1, 1_000_000_000_000];
-    static double PriceLongToDouble(long priceLong) => (double)priceLong / 1_000_000;
-    static decimal PriceLongToDecimal(long priceLong) => (decimal)priceLong / 1_000_000;
-
-
-    static Gen<(long PriceLong, long Quantity)> GenPriceQuantity(int priceMaxValue, int priceDecimalPlaces, long lineValueMaxValue)
-    {
-        var priceFactor = (long)Math.Pow(10, priceDecimalPlaces);
-        var genPriceLong = Gen.Long[1, priceMaxValue * priceFactor];
-        return genPriceLong.SelectMany(p =>
-        {
-            var quantityMax = lineValueMaxValue * priceFactor / p;
-            return Gen.Long[-quantityMax, quantityMax].Select(q => (p, q));
-        });
-    }
-
-    static string PriceLongToString(long priceLong, int dp = 6)
-    {
-        var s = priceLong.ToString();
-        if (s.Length < dp + 1)
-        {
-            return string.Concat("0.", s.PadLeft(dp, '0'));
-        }
-        var span = s.AsSpan();
-        var beforeDot = span[..^dp];
-        var afterDot = span.Slice(span.Length - dp, dp).TrimEnd('0');
-        return afterDot.Length == 0 ? new string(beforeDot) : string.Concat(beforeDot, ".", afterDot);
-    }
-
     [Fact]
-    public void PriceDoubleToString()
+    public void DoubleSupportsRoundtripPrecisionTo15()
     {
-        genPriceLong.Sample(
-              priceLong => PriceLongToDouble(priceLong).ToString() == PriceLongToString(priceLong)
-            , output.WriteLine);
-    }
-
-    [Fact]
-    public void PriceDecimalToString()
-    {
-        genPriceLong.Sample(
-              priceLong => PriceLongToDecimal(priceLong).ToString("0.######") == PriceLongToString(priceLong)
-            , output.WriteLine);
-    }
-
-    [Fact]
-    public void SumQuantityTimesPriceDoubleToString()
-    {
-        const int dp = 2;
-        var priceFactor = (long)Math.Pow(10, dp);
-        GenPriceQuantity(1_000_000, dp, 100_000_000_000)
-        .Array[2, 10_000]
-        .Sample(a =>
+        var genDigit = Gen.Char['0', '9'];
+        var genPriceIO = Gen.Int[1, 15]
+            .SelectMany(p =>
+                genDigit.Array[p + 1].Select(Gen.Int[0, p], (cs, d) =>
+                {
+                    cs[d] = '.';
+                    return new string(cs);
+                }));
+        genPriceIO.Sample(priceString =>
         {
-            var valueLong = a.Sum(i => i.PriceLong * i.Quantity);
-            var valueDouble = a.Select(i => (double)i.PriceLong / priceFactor * i.Quantity).NSum();
-            var doubleString = Math.Round(valueDouble, dp).ToString();
-            var longString = PriceLongToString(valueLong, dp);
-            return doubleString == longString;
+            var priceDouble = double.Parse(priceString);
+            return priceString.Trim('0').TrimEnd('.') == priceDouble.ToString("#.#################");
+        }, writeLine: output.WriteLine);
+    }
+
+    private static void DoubleSumPrecision(int significantFigures, int maxLength)
+    {
+        const double scaling = 0.01;
+        var lower = (long)Math.Pow(10, significantFigures - 1);
+        var upper = (long)Math.Pow(10, significantFigures) - 1;
+        Gen.Long[lower, upper].Array[100, maxLength]
+        .Sample(longs =>
+        {
+            var longSum = longs.Sum();
+            var doubleSum = longs.Sum(i => i * scaling);
+            return (doubleSum / scaling).ToString("#") == longSum.ToString();
         });
     }
 
     [Fact]
-    public void SumQuantityTimesPriceDecimalToString()
+    public void DoubleSumPrecision12()
     {
-        const int dp = 2;
-        var priceFactor = (long)Math.Pow(10, dp);
-        GenPriceQuantity(1_000_000, dp, 100_000_000_000)
-        .Array[2, 100_000]
-        .Sample(a =>
-        {
-            var valueLong = a.Sum(i => i.PriceLong * i.Quantity);
-            var valueDecimal = a.Select(i => (decimal)i.PriceLong / priceFactor * i.Quantity).Sum();
-            var decimalString = valueDecimal.ToString("0.##");
-            var longString = PriceLongToString(valueLong, dp);
-            return decimalString == longString;
-        });
+        DoubleSumPrecision(12, 350); // 9_999_999_999.99
+    }
+
+    [Fact]
+    public void DoubleSumPrecision11()
+    {
+        DoubleSumPrecision(11, 1_500); // 999_999_999.99
+    }
+
+    [Fact]
+    public void DoubleSumPrecision10()
+    {
+        DoubleSumPrecision(10, 7_100); // 99_999_999.99
+    }
+
+    [Fact]
+    public void DoubleSumPrecision9()
+    {
+        DoubleSumPrecision(9, 35_500); // 9_999_999.99
     }
 }
