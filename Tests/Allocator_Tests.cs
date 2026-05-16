14318286a65dd94d98c12a21ca2b9a3469b261ad
Anthony Lloyd <anthony@lloydhq.com>
2026-05-16 12:17:20 +0100

    remove IMToolsTests.cs

--- Tests/Allocator_Tests.cs
+++ Tests/Allocator_Tests.cs
@@ -4,9 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
-using System.Runtime.CompilerServices;
 using CsCheck;
-using ImTools;
 
 public class Allocator_Tests
 {
